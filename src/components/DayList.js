f60b5beca59f05983c215c05922d1866ef573f59
FelixPriori <felixriouxsabourin@gmail.com>
2020-03-16 18:50:48 -0400

    Implemented DayList to the app

---
+++ src/components/DayList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DayListItem from 'components/DayListItem';
+
+export default function DayList(props) {
+  const days = props.days;
+  const dayListItems = days.map( day => 
+    <DayListItem
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.setDay} 
+    />)
+  return (<ul>{dayListItems}</ul>);
+}
